0819565579cc269b6a4587d2a6c550ec0a45035b
渢鸞 <admin@crack.moe>
2023-11-23 21:28:12 +0800

    Reformat source structure & add more items (#11)
    * pr
    
    * 修改源码结构
    
    * fixup
    
    * add  more translation

---
+++ test/test.c
@@ -0,0 +1,31 @@
+bool *haku(int *a) {
+	a++;
+	char *b;
+	b=a;
+	return b;
+}
+void bug(){
+	register i[10];
+	i[0]=0;
+	i=0;
+}
+union X{
+};
+void mian() {
+	struct X x;
+	int w=((float)7.9);
+	printf("Hentai!");
+	int i=sizeof(int);
+	int a, b, c;
+	printf("%d", a);
+	haku(a, c);
+	const int f = 9 / 0;
+	bug(f);
+	const int i=1;
+	i=114514;
+	long long long verylong;
+	long xxx=NULL;
+	long xxxx=((void *)0);
+	char yyyy=114514;
+}
+#include<notexist.h>
